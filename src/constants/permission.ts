a44cbc5289712dab0d0ade4f0c3a4bd3c747f3f6
MohsenSaf <moh.safi.1380@gmail.com>
2025-08-07 19:21:27 +0330

    change permissions' title format

--- src/constants/permission.ts
+++ src/constants/permission.ts
@@ -1,26 +1,26 @@
 export const PERMISSIONS = [
   {
-    name: 'ROLE:CREATE',
-    description: 'Allows the user to create new roles',
+    name: "Role:Create",
+    description: "Allows the user to create new roles",
   },
   {
-    name: 'ROLE:UPDATE',
-    description: 'Allows the user to update existing roles',
+    name: "Role:Update",
+    description: "Allows the user to update existing roles",
   },
   {
-    name: 'ROLE:DELETE',
-    description: 'Allows the user to delete roles',
+    name: "Role:Delete",
+    description: "Allows the user to delete roles",
   },
   {
-    name: 'USER:CREATE',
-    description: 'Allows the user to create new users',
+    name: "User:Create",
+    description: "Allows the user to create new users",
   },
   {
-    name: 'USER:UPDATE',
-    description: 'Allows the user to update existing users',
+    name: "User:Update",
+    description: "Allows the user to update existing users",
   },
   {
-    name: 'USER:DELETE',
-    description: 'Allows the user to delete users',
+    name: "User:Delete",
+    description: "Allows the user to delete users",
   },
-];
+]
